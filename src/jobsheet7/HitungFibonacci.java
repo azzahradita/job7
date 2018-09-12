363c87d3d996577706867b694d36b5774aabac1b
pc <pc@LAPTOP-D93TPG0A>
2018-09-12 10:53:27 +0700

    Initial Commit

--- src/jobsheet7/HitungFibonacci.java
+++ src/jobsheet7/HitungFibonacci.java
@@ -1,6 +1,4 @@
 package jobsheet7;
-import java.math.BigInteger;
-import java.util.Scanner;
 public class HitungFibonacci {
     private static void tampilJudul(String identitas)
     {
@@ -15,37 +13,5 @@ public class HitungFibonacci {
         String identitas = "Az Zahra Dita Pratiwi / X RPL 3 / 9";
     
         tampilJudul(identitas);
-        int n = tampilInput();
-        BigInteger hasil = fibo(n);
-        tampilHasil(n, hasil);
-    }
-    
-    private static int tampilInput()
-    {
-        Scanner scanner = new Scanner(System.in);
-        
-        System.out.print("Bilangan ke-: ");
-        int n = scanner.nextInt();
-        
-        return n;
-    }
-    
-    private static BigInteger fibo(int n)
-    {
-        BigInteger[] hasil = new BigInteger[n];
-        
-        hasil[0] = BigInteger.ONE;
-        hasil[1] = BigInteger.ONE;
-        
-        for (int i = 2; i < n; i++) {
-            hasil[i] = hasil[i-1].add(hasil[i-2]);
-        }
-        
-        return hasil[n-1];
-    }
-    
-    private static void tampilHasil(int n, BigInteger hasil)
-    {
-        System.out.println("Bilangan Fibonacci ke-" + n +" : " + hasil);
     }
 }
