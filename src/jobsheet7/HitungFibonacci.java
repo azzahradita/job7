50b5a0827abcc36638e54a5e945e78d5c9b39762
pc <pc@LAPTOP-D93TPG0A>
2018-09-12 09:28:03 +0700

    Initial Commit

---
+++ src/jobsheet7/HitungFibonacci.java
@@ -0,0 +1,18 @@
+package jobsheet7;
+public class HitungFibonacci {
+    private static void tampilJudul(String identitas)
+    {
+        System.out.println("Identitas : "+ identitas);
+        
+        System.out.println("\nHitung Fibonaccci");
+        System.out.println("1, 1, 2, 3, 5, 8, 13, 21, ... dst.\n");
+    }
+    
+    public static void main(String[] args) 
+    {
+        String identitas = "Az Zahra Dita Pratiwi / X RPL 3 / 9";
+    
+        tampilJudul(identitas);
+    }
+    
+}
